ea8e0258f2c82e2e61ee0878c28f217bcd369d69
Shabari <shabaridas056@gmail.com>
2026-03-23 21:41:19 +0530

    refactor: use list for SQL conditions in Student Activity Statement report

--- general_activity_manager/general_activity_manager/report/student_activity_statement/student_activity_statement.py
+++ general_activity_manager/general_activity_manager/report/student_activity_statement/student_activity_statement.py
@@ -22,33 +22,40 @@ def get_columns():
 def get_data(filters):
 	if not filters:
 		filters = {}
-	conditions = "WHERE docstatus < 2"
+
+	conditions = ["docstatus < 2"]
 	user_roles = frappe.get_roles(frappe.session.user)
+
 	if "Department Head" in user_roles or "System Manager" in user_roles:
 		pass
 	elif "Faculty" in user_roles:
 		faculty_dept = frappe.db.get_value("Faculty", {"user": frappe.session.user}, "department")
 		if faculty_dept:
-			conditions += " AND department = " + repr(faculty_dept)
+			conditions.append("department = " + repr(faculty_dept))
 	elif "Student" in user_roles:
 		student_name = frappe.db.get_value("Student", {"user": frappe.session.user}, "name")
 		if student_name:
-			conditions += " AND participant = " + repr(student_name)
+			conditions.append("participant = " + repr(student_name))
+
 	if filters.get("participant_type"):
-		conditions += " AND participant_type = " + repr(filters.get("participant_type"))
+		conditions.append("participant_type = " + repr(filters.get("participant_type")))
 	if filters.get("participant"):
-		conditions += " AND participant LIKE " + repr("%" + filters.get("participant") + "%")
+		conditions.append("participant LIKE " + repr("%" + filters.get("participant") + "%"))
 	if filters.get("department"):
-		conditions += " AND department = " + repr(filters.get("department"))
+		conditions.append("department = " + repr(filters.get("department")))
 	if filters.get("category"):
-		conditions += " AND category = " + repr(filters.get("category"))
+		conditions.append("category = " + repr(filters.get("category")))
 	if filters.get("from_date"):
-		conditions += " AND event_date >= " + repr(filters.get("from_date"))
+		conditions.append("event_date >= " + repr(filters.get("from_date")))
 	if filters.get("to_date"):
-		conditions += " AND event_date <= " + repr(filters.get("to_date"))
+		conditions.append("event_date <= " + repr(filters.get("to_date")))
+
+	where_clause = " WHERE " + " AND ".join(conditions) if conditions else ""
+
 	query = (
-		"SELECT participant_type, participant, department, event_name, category, event_date, certificate FROM `tabActivity Management` "
-		+ conditions
+		"SELECT participant_type, participant, department, event_name, category, event_date, certificate "
+		"FROM `tabActivity Management` "
+		+ where_clause
 		+ " ORDER BY participant, event_date DESC"
 	)
 	return frappe.db.sql(query, as_dict=True)
